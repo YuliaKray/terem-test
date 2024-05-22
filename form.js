b0d6e5d9b8f4bada881a8cc8cb3dd9edf0ed4a97
Yulia Kraynova <ungo1998s@yandex.ru>
2024-05-22 17:34:11 +0300

    работа над ошибками

--- form.js
+++ form.js
@@ -2,46 +2,40 @@ const submitButton = document.querySelector('button');
 const formElement = document.querySelector('form');
 const textElement = document.querySelector('p');
 
+
 // Функция для ассинхронного отправления GET-запроса
 function api() {
     return fetch(`/api/form`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'multipart/form-data'
+            'Content-Type': 'text/plain'
         },
     })
         .then((res) => {
             if (res.ok) {
-                return res.json()
+                return res.json();
             } else {
-                return Promise.reject(`Ошибка: ${res.status}`)
+                return Promise.reject(`Ошибка: ${res.status}`);
             }
         })
         .then((res) => {
-                alert(res)
+            alert(res);
         })
         .catch((err) => {
-            alert(err)
+            alert(err);
         })
-}
+};
 
 // Функция отправки данных формы
 function submitForm(event) {
     event.preventDefault();
 
-    const data = {
-        list1: formElement.querySelector('[name="list-1"]').value,
-        list2: formElement.querySelector('[name="list-2"]').value,
-        list3: formElement.querySelector('[name="list-3"]').value,
-        list4: formElement.querySelector('[name="list-4"]').value,
-        list5: formElement.querySelector('[name="list-5"]').value,
-        name: formElement.querySelector('[name="name"]').value,
-        description: formElement.querySelector('[name="description"]').value
-    };
-    console.log(data);
+    const data = new FormData(formElement);
+    const values = Object.fromEntries(data.entries());
+
     api();
-    textElement.textContent = JSON.stringify(data);
-}
+    textElement.textContent = JSON.stringify(values); 
+};
 
 // Слушатель события отправки данных формы
-formElement.addEventListener('submit', submitForm)
+formElement.addEventListener('submit', submitForm);
--- index.js
+++ index.js
@@ -3,30 +3,31 @@ const header = document.querySelector('.header');
 
 const secondButton = document.querySelector('.btn-success');
 const main = document.querySelector('.main');
+const secondBox = document.querySelector('.main__box_dashed');
+
 
 const popupButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 
 // Функция для нажатия кнопки "Кнопка 1", чтобы скрывался блок с заголовком
 function handleClickFirstButton() {
-    header.classList.toggle('header_invisible');
-}
+    header.classList.toggle('invisible');
+};
 
 // Функция для нажатия кнопки "Кнопка 2", чтобы средний блок во втором ряду менялся местами с левым блоком
 function handleClickSecondButton() {
-    const mainItems = main.children;
-    main.prepend(mainItems[1])
-}
+    secondBox.classList.toggle('order');
+};
 
 // Функция для закрытия попапа
 function closePopup() {
-    popup.classList.remove('popup_open');
-}
+    popup.classList.toggle('invisible');
+};
 
 
 // Слушатели событий для кнопок
 firstButton.addEventListener('click', handleClickFirstButton);
 
-secondButton.addEventListener('click', handleClickSecondButton)
+secondButton.addEventListener('click', handleClickSecondButton);
 
 popupButton.addEventListener('click', closePopup);
